23cb048cd21fa3c326ab03ef6c2b4b51c1f792bb
isoopod <isoopod@proton.me>
2026-03-19 00:26:50 +1300

    chore: fix scripts after changing rojo project names

--- scripts/shell/test.sh
+++ scripts/shell/test.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-rojo build dev.project.json --output dist.rbxl
+rojo build default.project.json --output dist.rbxl
 python3 scripts/python/upload_and_run_task.py dist.rbxl tests/TestRunner.luau
 rm dist.rbxl
